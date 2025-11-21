fb053f904e24fe2a22a6fc94c462dbaeef4fb3c0
NickyGee44 <nick@bridg3.io>
2025-11-21 12:44:52 -0500

    Fix all warning icon SVG expansion issues - Add inline style constraints (width: 16px, height: 16px) to all w-4 h-4 warning icons - Add max-w and max-h Tailwind classes to all warning icons - Add flex-shrink-0 to all warning icons - Update global CSS to handle w-4 h-4 SVGs with !important rules - Fix warning icons in BasicsSection, MetricsSection, FormActions, and NarrativeSection

--- components/form/BasicsSection.tsx
+++ components/form/BasicsSection.tsx
@@ -43,7 +43,7 @@ export default function BasicsSection() {
         />
         {!config.title && (
           <p className="mt-2 text-xs text-yellow-400/80 flex items-center space-x-1">
-            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 max-w-[16px] max-h-[16px] flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" style={{ width: '16px', height: '16px', flexShrink: 0 }}>
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
             <span>This field is required</span>
@@ -68,7 +68,7 @@ export default function BasicsSection() {
         />
         {!config.clientName && (
           <p className="mt-2 text-xs text-yellow-400/80 flex items-center space-x-1">
-            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 max-w-[16px] max-h-[16px] flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" style={{ width: '16px', height: '16px', flexShrink: 0 }}>
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
             <span>This field is required</span>
@@ -116,7 +116,7 @@ export default function BasicsSection() {
         </select>
         {!config.industry && (
           <p className="mt-2 text-xs text-yellow-400/80 flex items-center space-x-1">
-            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 max-w-[16px] max-h-[16px] flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" style={{ width: '16px', height: '16px', flexShrink: 0 }}>
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
             <span>This field is required</span>
